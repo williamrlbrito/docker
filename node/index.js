72c709bce0269d583a3b1d3a1b003f1d4a2559df
William Brito <williamrldb@gmail.com>
2022-01-14 17:26:58 -0400

    --Colocando em prática

---
+++ node/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("<h1>Full Cycle</h1>"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
